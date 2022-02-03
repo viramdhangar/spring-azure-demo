dc33c9159b7a75d7f164c6bef7d7fb3c2ade3224
Viram Dhangar <viram.dhangar@gmail.com>
2022-02-03 22:15:06 +0530

    my first commit

---
+++ src/main/java/com/docker/springdockerd/controller/Welcome.java
@@ -0,0 +1,13 @@
+package com.docker.springdockerd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "Welcome boot";
+	}
+}
